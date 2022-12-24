be0399ba97cd802bdf533d984d4f4450592c0d95
Burr Sutter <burrsutter@gmail.com>
2022-12-24 10:34:41 -0500

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Java, Quarkus, Hello, NO Dockerfile
+```
+mvn package
+
+java -jar target/java-quarkus-hello-1.0.0-SNAPSHOT-runner.jar
+
+curl localhost:8080
+
+Hello Quarkus 4 12-23-2022 03:13:59 on unknown
+```
+
---
+++ src/main/java/com/burrsutter/GreetingResource.java
@@ -0,0 +1,46 @@
+package com.burrsutter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Date;
+
+
+@Path("/")
+public class GreetingResource {
+    int cnt = 1;
+    private static final String HOSTNAME = 
+        System.getenv().getOrDefault("HOSTNAME", "unknown");
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String root() {
+        Locale locale;
+        DateFormat dateFormat; 
+        String pattern; 
+        SimpleDateFormat simpleDateFormat; 
+    
+        locale = new Locale("en", "US");
+        dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+        // pattern = "dd-M-yyyy hh:mm:ss"; // euro
+        pattern = "M-dd-yyyy hh:mm:ss"; // us
+        simpleDateFormat = new SimpleDateFormat(pattern);
+        String stringNow = simpleDateFormat.format(new Date());
+
+        System.out.println("hello: " + cnt);
+        return "Hello Quarkus " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+    }
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello";
+    }
+}
