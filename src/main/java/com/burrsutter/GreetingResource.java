cdb34b91bf69f2cca0d2f34e6c5d8f6b161041f2
Burr Sutter <burrsutter@gmail.com>
2023-03-25 18:06:42 -0400

    Aloha

--- src/main/java/com/burrsutter/GreetingResource.java
+++ src/main/java/com/burrsutter/GreetingResource.java
@@ -33,8 +33,8 @@ public class GreetingResource {
         simpleDateFormat = new SimpleDateFormat(pattern);
         String stringNow = simpleDateFormat.format(new Date());
 
-        System.out.println("hello: " + cnt);
-        return "Hello Quarkus " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+        System.out.println("Aloha: " + cnt);
+        return "Aloha Quarkus " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     @GET
